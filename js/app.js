44bbb3fd422207a658c2f6cd4ec3d1321768258f
Sebastian <sebastianmurphy516@gmail.com>
2024-07-19 18:21:14 -0400

    Mostly finished. Code page needs to be filled out with full commenting and description

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ function getTable() {
   amount = Number.parseFloat(amount);
   term = Number.parseInt(term);
   rate = Number.parseFloat(rate);
-  if (amount == NaN || term == NaN || rate == NaN) {
-    alert("Please input valid number");
-  } else {
+  if (Number.parseFloat(amount) && Number.isInteger(term) && Number.parseFloat(rate)) {
     // Float, Int, Float
     let months = calcLoan(amount, term, rate);
     buildCard(months);
     buildTable(months);
+  } else {
+    alert("Please input valid number");
   }
 }
 function calcLoan(amount, term, rate) {
@@ -47,18 +47,50 @@ function calcLoan(amount, term, rate) {
   return months;
 }
 function buildCard(data) {
-    // Grab Data
-    let monPayment = data[0].payment.toFixed(2);
-    let totalCost = data[0].tOwed.toFixed(2);
-    let totalInterest = data[data.length-1].tInterest.toFixed(2);
-    let totalPrincipal = data[0].remBalance.toFixed(2);
-    // Grab Elements
-    let target = document.getElementById("card-template-target");
-    let template = document.importNode(document.getElementById("payment-card-template").content, true);
-    template.querySelector("[data-monthly]").textContent = monPayment;
-    template.querySelector("[data-total-principal]").textContent = "$" + totalPrincipal;
-    template.querySelector("[data-total-interest]").textContent = "$" + totalInterest;
-    template.querySelector("[data-total-cost]").textContent = "$" + totalCost;
-    target.appendChild(template);
+  // Grab Data
+  let monPayment = data[0].payment.toFixed(2);
+  let totalCost = data[0].tOwed.toFixed(2);
+  let totalInterest = data[data.length - 1].tInterest.toFixed(2);
+  let totalPrincipal = data[0].remBalance.toFixed(2);
+  // Grab Elements
+  let target = document.getElementById("card-template-target");
+  target.innerHTML = "";
+  let template = document.importNode(
+    document.getElementById("payment-card-template").content,
+    true
+  );
+  template.querySelector("[data-monthly]").textContent = monPayment;
+  template.querySelector("[data-total-principal]").textContent =
+    "$" + totalPrincipal;
+  template.querySelector("[data-total-interest]").textContent =
+    "$" + totalInterest;
+  template.querySelector("[data-total-cost]").textContent = "$" + totalCost;
+  target.appendChild(template);
 }
-function buildTable(data) {}
+function buildTable(months) {
+  // Grab Elements
+  let target = document.getElementById("table-row-target");
+  target.innerHTML = "";
+  months.forEach((index) => {
+    let template = document.importNode(
+      document.getElementById("table-row-template").content,
+      true
+    );
+    let month = template.querySelector("[data-month]");
+    let payment = template.querySelector("[data-payment]");
+    let principal = template.querySelector("[data-principal]");
+    let interest = template.querySelector("[data-interest]");
+    let totalInterest = template.querySelector("[data-total-interest]");
+    let balance = template.querySelector("[data-balance]");
+    if (index.month == 0) {
+    } else {
+      month.textContent = index.month;
+      payment.textContent = "$" + index.payment.toFixed(2);
+      principal.textContent = "$" + index.priPayment.toFixed(2);
+      interest.textContent = "$" + index.intPayment.toFixed(2);
+      totalInterest.textContent = "$" + index.tInterest.toFixed(2);
+      balance.textContent = "$" + index.remBalance.toFixed(2);
+      target.appendChild(template);
+    }
+  });
+}
